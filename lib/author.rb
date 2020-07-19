cc21df04fc24004abf0d42084423d9bceea95e55
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-07-19 20:02:32 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,29 +1,34 @@
 class Author
-
-  attr_accessor :name,:posts
-
-  @@post_count = 0
-
+  
+  attr_accessor :name
+  
+  @@all_authors =[]
+  
   def initialize(name)
     @name = name
-    @posts = []
+    @@all_authors << self
   end
-
+  
   def add_post(post)
-    @posts << post
     post.author = self
-    @@post_count += 1
   end
-
+  
   def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+    post = Post.new (title)
+    add_post(post)
+  end
+  
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 
   def self.post_count
-    @@post_count
+    post_count = 0
+    @@all_authors.each do |author|
+      array = author.posts
+      post_count += array.length
+    end
+    post_count
   end
 
 end
